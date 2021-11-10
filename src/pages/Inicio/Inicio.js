fd663bd3d9997cae39dee8a997e098d58b9ecca2
HuitziDomingo <huitzitzilidomingo@gmail.com>
2021-11-09 23:30:28 -0600

    configuracion v1

---
+++ src/pages/Inicio/Inicio.js
@@ -0,0 +1,12 @@
+
+import './Inicio.scss'
+
+const Inicio = () => {
+    return (
+        <div className="bg-img">
+          <button className="btn btn-primary" >Inicio</button>
+        </div>
+    )
+}
+
+export default Inicio
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Inicio } from './Inicio/Inicio'
+export { default as Form } from './Form/Form'
